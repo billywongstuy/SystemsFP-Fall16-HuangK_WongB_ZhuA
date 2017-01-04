1e425d7a9658770dc9677a140e6a3287353c32e8
Billy Wong <bwong5@stuy.edu>
2017-01-04 18:09:43 -0500

    more setup

--- cards.c
+++ cards.c
@@ -32,17 +32,60 @@ void setupDeck(struct card * deck) {
   }
 }
 
+
+int suitNum(card c) {
+  if (c.suit == 'D') {
+    return 0;
+  }
+  else if (c.suit = 'C') {
+    return 1;
+  }
+  else if (c.suit = 'H') {
+    return 2;
+  }
+  else if (c.suit = 'S') {
+    return 3;
+  }
+  return -1;
+}
+
+
+int valNum(card c) {
+  if (c.value <= 2) {
+    return c.value + 13;
+  }
+  return c.value;
+}
+
+//special cases for 2 and A
+card greaterCard(struct card c1, struct card c2) {
+  int c1Suit = suitNum(c1);
+  int c2Suit = suitNum(c2);
+  int c1Val = valNum(c1);
+  int c2Val = valNum(c2);
+  
+  
+  if (c1Val == c2Val) {
+    if (c1Suit >= c2Suit) {
+      return c1;
+    }
+    return c2;
+  }
+  else if (c1Val > c2Val) {
+    return c1;
+  }
+  return c2;
+}
+
 int main() {
   struct card test;
-  setupDeck(deck);
-  
   test.value = 4;
   test.suit = 'S';
+  
+  setupDeck(deck);
 
-  printf("Card info: %d of %c\n",test.value,test.suit);
-  for (int i = 0; i < sizeof(deck)/sizeof(deck[i]); i++) {
-    printf("Card %d: %d of %c\n",i,deck[i].value,deck[i].suit);
-    
-  }
+  struct card g = greaterCard(deck[0],deck[23]);
+  
+  printf("Card %d of %c\n",g.value,g.suit);
   
 }
