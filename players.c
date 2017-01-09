30b512ebf42463e6464108727b459b101acacece
Billy Wong <bwong5@stuy.edu>
2017-01-09 15:33:15 -0500

    commit

--- players.c
+++ players.c
@@ -4,13 +4,14 @@
 #include "players.h"
 
 
-void initializePlayer(struct player *p) {
-  p->cardsLeft = 13;
+void initializePlayers(struct player **p, int numP) {
+  int i;
 
-  //getting specific ids is later
-  p->id = 0;
+  for (i = 0; i < numP; i++) {
+    p[i]->cardsLeft = 13;
+    p[i]->id = i;
+  }
 }
-
 
 
 void cardSwap(struct card *deck, int in1, int in2) {
@@ -19,39 +20,27 @@ void cardSwap(struct card *deck, int in1, int in2) {
   deck[in2] = temp;
 }
 
-void distributeCards(struct card * deck, struct player *p1, struct player *p2, struct player *p3, struct player *p4) {
+void distributeCards(struct card * deck, struct player ** players, int numP) {
   int deckCardsLeft = 52;
-  int i;
+  int i,j;
   int random;
 
-  initializePlayer(p1);
-  initializePlayer(p2);
-  initializePlayer(p3);
-  initializePlayer(p4);
+
+  initializePlayers(players,numP);
   
-  for (i = 0; i < 13; i++) {
-    random = rand()%deckCardsLeft;   
-    p1->hand[i] = deck[random];
-    cardSwap(deck,random,deckCardsLeft-1);
-    deckCardsLeft--;
+  for (i = 0; i < numP; i++) {
 
-    random = rand()%deckCardsLeft;   
-    p2->hand[i] = deck[random];
-    cardSwap(deck,random,deckCardsLeft-1);
-    deckCardsLeft--;
-
-    random = rand()%deckCardsLeft;   
-    p3->hand[i] = deck[random];
-    cardSwap(deck,random,deckCardsLeft-1);
-    deckCardsLeft--;
-
-
-    random = rand()%deckCardsLeft;   
-    p4->hand[i] = deck[random];
-    cardSwap(deck,random,deckCardsLeft-1);
-    deckCardsLeft--;
-    
-    
+    for (j = 0; j < 13; j++) {
+      
+      random = rand()%deckCardsLeft;
+      
+      players[i]->hand[j] = deck[random];
+      cardSwap(deck,random,deckCardsLeft-1);
+      deckCardsLeft--;
+      
+    }
   }
-
+      
+    
 }
+
