cbd79f184520d66b99661fc4d7afca35168f5a93
Karen Huang <khuang428@gmail.com>
2017-01-20 15:32:29 -0500

    header things

--- rules.h
+++ rules.h
@@ -7,7 +7,9 @@
 
 //0 is success
 //other numbers for different why invalid
-int validMove(int lenC, int lenL, int * choice, int * last);
+int no3D(int lenC, int *choice);
+
+int validMove(int turn, int lenC, int lenL, int * choice, int * last);
 
 int validSingle(int * choice, int * last);
 
@@ -30,12 +32,13 @@ int checkBomb(int * cards);
 int checkSF(int * cards);
 
 char * getInvalidMessage(int e);
+//-3 START WITH 3 OF DIAMONDS
 //-2 what are you throwing out man...
 //-1 wrong amount of cards *except for first turn and freebies!!!
 // 0 all good 
 // 1 hand not strong enough
 // 2 cards are not matching
-//need who goes first and when game ends
+//need when game ends
 
 #endif
 
